d6a99bf7100945fd224b0f9ada22410c5934a3cd
황도영 <dodo45133@gmail.com>
2022-02-27 14:28:19 +0900

    [핵심 pick/실습 코드 릴레이] 황도영 7주차 (#49)
    * 기본 resource
    
    * create member
    
    * member list
    
    * member list
    
    * product list
    
    * update product
    
    * i오타
    
    * uri 앞엔 항상 / 로 시작하자

---
+++ src/main/java/jpabook/jpashop/controller/BookForm.java
@@ -0,0 +1,20 @@
+package jpabook.jpashop.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+@Setter
+public class BookForm {
+
+    @NotEmpty(message = "비어있음 안댐")
+    private long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+    private String author;
+    private String isbn;
+}
